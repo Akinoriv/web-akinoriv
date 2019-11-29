14979948e940fa3f446003c891e8ea0521fa45f5
Вироника Воднева <akinoriv@MacBook-Air-Vironika.local>
2019-11-29 19:45:25 +0300

    поменяла структуру пректа ))

---
+++ app/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.use(express.static("Public"));
+
+//app.get('/', function(req, res){
+   // res.send('Hello World');
+  // res.sendFile('./Public/index.html', { root: '.' });
+//});
+
+app.listen(3000);
