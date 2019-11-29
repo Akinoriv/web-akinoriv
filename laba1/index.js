341fc31c12872bc4dbcbf4cf1d3f7528e03d25b7
Вироника Воднева <akinoriv@MacBook-Air-Vironika.local>
2019-11-29 19:08:07 +0300

    прокинула на localehost

--- laba1/index.js
+++ laba1/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+app.use(express.static("Public"));
 
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
+//app.get('/', function(req, res){
+   // res.send('Hello World');
+  // res.sendFile('./Public/index.html', { root: '.' });
+//});
 
 app.listen(3000);
